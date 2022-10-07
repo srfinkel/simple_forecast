9a0dee8e1e576eddcc7f83d015d1c57f325855f6
Sarah Ruth Finkel <srfinkel@gmail.com>
2022-10-06 22:42:26 -0600

    updated Location model and added views

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,9 @@
 class Location < ApplicationRecord
-	validates :address, presence: true
-	validates :latitude, presence: true
-	validates :longitude, presence: true
+	validates :address, 		presence: true
+	validates :latitude, 		presence: true
+	validates :longitude, 	presence: true
+
+	def format_address(address)
+		self.address.gsub(/[', ']/, ',' => '%2C', ' ' => '%20')
+	end
 end
