c70f1d76a58d36154382912abda7c888c74dbfcc
Sarah Ruth Finkel <srfinkel@gmail.com>
2022-10-07 08:20:54 -0600

    implementing turbo frames to show the latest forecast on the index page. Currently showing just the coordinates of the last Location

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,18 +1,24 @@
 class LocationsController < ApplicationController
   
 	def index
+		@location = Location.last
+	end
+
+	def new
 		@location = Location.new
 	end
 
 	def create
 		@location = Location.new(location_params)
-		@coordinates = Geocode.request(@location)
-		logger.info "Coordinates: #{@corrdinates}"
-		@location.update(location_params.merge(latitude: @coordinates.first, longitude: @coordinates.last))
-  end
+		coordinates = Geocode.request(@location)
 
-	def show
-	end
+		@location.update(location_params.merge(latitude: coordinates.first, longitude: coordinates.last))
+
+		respond_to do |format|
+			format.html { redirect_to locations_path, notice: "We were able to get your forecast" }
+			format.turbo_stream
+		end
+  end
 
 	private
 
